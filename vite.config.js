525c72faca97c1a44bdf930fa04273bebe3f0f98
wdog <wdog666@gmail.com>
2025-12-18 23:48:53 +0100

    starting point

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [...refreshPaths,
+                "app/Livewire/**",
+                "app/Filament/**",
+                "app/Providers/Filament/**",
+                "resources/views/**"
+            ],
         }),
         tailwindcss(),
     ],
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+            https: true
+        }
     },
 });
